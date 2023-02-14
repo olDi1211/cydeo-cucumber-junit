03b2e736cefe54f515c0ae85d0572e023f2096d3
Dmytro Oleksiiuk <dmytrooleksiiuk@gmail.com>
2023-02-14 20:51:50 +0100

    Cucumber is installed

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,68 @@
+package com.cydeo.utilities;
+
+import com.sun.jna.WString;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class BrowserUtils {
+
+
+    //This given method will accept int (in seconds) and execute Thread.sleep for given duration
+    public static void sleep(int second){
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        }catch (InterruptedException e){
+
+        }
+    }
+
+    /*
+    This method accepts 3 arguments.
+    Arg1: WebDriver
+    Arg2: expectedInUrl : for verify if the url contains given String.
+      - If condition matches, will break loop.
+    Arg3: expectedTitle to be compared against actualTitle
+     */
+    public static void switchWindowAndVerify(String expectedInUrl, String expectedTitle){
+
+        Set<String> allWindowsHandles = Driver.getDriver().getWindowHandles();
+
+        for (String each : allWindowsHandles){
+            Driver.getDriver().switchTo().window(each);
+
+            System.out.println("Current URL: "+Driver.getDriver().getCurrentUrl());
+            if (Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+
+        }
+
+//5. Assert: Title contains expectedTitle
+        String currentTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(currentTitle.contains(expectedTitle));
+
+    }
+
+    //This method accepts a String "expectedTitle" and Asserts if it is true
+    public static void verifyTitle(String expectedTitle){
+
+        Assert.assertEquals(Driver.getDriver().getTitle(),expectedTitle);
+
+    }
+
+    //Creating a utility method for ExplicitWait so we don't have to repeat the lines
+    public static void waitForInvisibilityOf (WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+    }
+
+
+}
